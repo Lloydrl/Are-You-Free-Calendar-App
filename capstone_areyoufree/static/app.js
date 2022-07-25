4858699b90b218471bacb044df0f5b9b32d4fc7b
Rachel Lloyd <rachellloyd@Rachels-MacBook-Pro.local>
2022-07-25 13:34:25 -0400

    capstone

--- capstone_areyoufree/static/app.js
+++ capstone_areyoufree/static/app.js
@@ -1,45 +1,42 @@
-import dayjs from dayjs
-import weekday from dayjs/plugin/weekday
-import weekofYear from dayjs/plugin/weekofYear
-
 Vue.component('month-indicator', {
-    template:
-        <div>
-            {{ selectedMonth }}
-        </div>
-    ,
+    // template:
+    //     <div>
+    //         {{ selectedMonth }}
+    //     </div>
+    // ,
 })
 
 Vue.component('month-selector',{
-    template:
-    <div>
-        {/* calendar date selector (pagination between months) */}
-    </div>
+    // template:
+    // <div>
+    //     {/* calendar date selector (pagination between months) */}
+    // </div>
 })
 
 Vue.component('weekdays', {
     template:
-    <div>
-        {/* shows the name of the weekdays */}
-    </div>
+        `<div>
+            <p>date.day()</p>
+        </div>`
 })
 
 Vue.component('day', {
-    template:
-    <div>
-        {/* shows single day of the month */}
-    </div>
+    // template:
+    // <div>
+    //     {/* shows single day of the month */}
+    // </div>
 })
 
 
 new Vue ({
     el: '#app',
     data: {
-        days: [],
-        months: [],
-        years: [],
     },
     methods: {
 
     }
 })
+
+let date = dayjs()
+
+console.log(date.day(0))
