4c57d6950b38a27ed280fb918f7e53bc2c9af3c0
Rachel Lloyd <rachel@Rachels-iMac.local>
2022-07-26 20:49:26 -0400

    capstone

--- capstone_areyoufree/static/app.js
+++ capstone_areyoufree/static/app.js
@@ -4,7 +4,12 @@ let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('e
 
 let calendar = document.getElementById('calendar')
 let newEvent = document.getElementById('new-event-modal')
+let deleteEventModal = document.getElementById('delete-modal')
 let backDrop = document.getElementById('modal-back-drop')
+let eventTitleInput = document.getElementById('event-title-input')
+let startTimeInput = document.getElementById('start-time-input')
+let endtTimeInput = document.getElementById('end-time-input')
+let eventNotes = document.getElementById('event-notes')
 let weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
 function openModal(date) {
@@ -13,7 +18,11 @@ function openModal(date) {
     let eventForDay = events.find(e => e.date === clicked)
     // add save and cancel buttons for event (if event exists)
     if (eventForDay) {
-
+        document.getElementById('event-title').innerText = eventForDay.title
+        document.getElementById('event-start-time'). innerText = eventForDay.startTime
+        document.getElementById('event-end-time'). innerText = eventForDay.endTime
+        document.getElementById('event-notes-sec'). innerText = eventForDay.notes
+        deleteEventModal.style.display = 'block'
     } else {
         // if event doesn't exist
         newEvent.style.display = 'block'
@@ -53,13 +62,28 @@ function load() {
         let dayBox = document.createElement('div')      //creates a box for each day of the month (in a div)
         dayBox.classList.add('day')
 
+        let dayString = `${month + 1}/${i - paddingDays}/${year}`
+
         // Determine if a padding day or an actual day is rendered
         if (i > paddingDays) {
             //specify dayBox
             dayBox.innerText = i - paddingDays
 
+            let eventForDay = events.find(e => e.date === dayString)
+
+            if (i - paddingDays === day && nav === 0) {
+                dayBox.id = 'current-day'
+            }
+
+            if (eventForDay) {
+                let eventDiv = document.createElement('div')
+                eventDiv.classList.add('event')
+                eventDiv.innerText = eventForDay.startTime + ': ' + eventForDay.title
+                dayBox.appendChild(eventDiv)
+            }
+
             //allow for user to click on dayBox (to create event!!!!!!!)
-            dayBox.addEventListener('click', () => openModal(`${month + 1}/${i - paddingDays}/${year}`))
+            dayBox.addEventListener('click', () => openModal(dayString))
         } else {
             //specify padding day
             dayBox.classList.add('padding')
@@ -70,6 +94,40 @@ function load() {
 
     console.log(paddingDays)
 
+}
+
+function closeModal() {
+    eventTitleInput.classList.remove('error')
+    newEvent.style.display = 'none'
+    deleteEventModal.style.display = 'none'
+    backDrop.style.display = 'none'
+    eventTitleInput.value = ''
+    clicked = null
+    load()
+}
+
+function saveEvent() {
+    if (eventTitleInput.value) {
+        eventTitleInput.classList.remove('error')
+        events.push({
+            date: clicked,
+            title: eventTitleInput.value,
+            startTime: startTimeInput.value,
+            endTime: endtTimeInput.value,
+            notes: eventNotes.value,
+        })
+
+        localStorage.setItem('events', JSON.stringify(events))
+        closeModal()
+    } else {
+        eventTitleInput.classList.add('error')
+    }
+}
+
+function deleteEvent() {
+    events = events.filter(e => e.date !== clicked)
+    localStorage.setItem('events', JSON.stringify(events))
+    closeModal()
 }
 
 function initButtons() {
@@ -84,6 +142,11 @@ function initButtons() {
         nav--
         load()
     })
+
+    document.getElementById('save-btn').addEventListener('click', saveEvent)
+    document.getElementById('cancel-btn').addEventListener('click', closeModal)
+    document.getElementById('delete-btn').addEventListener('click', deleteEvent)
+    document.getElementById('close-btn').addEventListener('click', closeModal)
 }
 
 initButtons()
